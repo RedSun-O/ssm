b345bb6e1a5eeb7ba9ec2eec171896c9d27b103e
user <2208867096@qq.com>
2021-02-11 11:14:25 +0800

    分支提交

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -14,4 +14,16 @@ public class Main {
 		System.out.println("HelloWorld!");
 		System.out.println("HelloWorld!");
     }
+	 @Test
+    public void testHelloWrold01() {
+        System.out.println("HelloWorld!");
+		System.out.println("HelloWorld!");
+		System.out.println("HelloWorld!"); 
+		System.out.println("HelloWorld!");
+
+		System.out.println("Hello分公司的风格World!");
+		System.out.println("Hell发地方个oWorld!");
+		System.out.println("HelloWorld!");
+		System.out.println("HelloWorld!");
+    }
 }
